923cac95fa56770b4b3e443f635151e427b7cf0e
杨泽 <1083849984@qq.com>
2021-02-25 10:47:38 +0800

    登录页面完成

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,6 @@
     });
 </script>
 
-<style>
-    /*每个页面公共css */
+<style lang="scss">
+@import "~@/style/reset.scss";
 </style>
---
+++ src/pages/login/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <view class="page-container">
+    <h1 class="title">后台管理系统</h1>
+    <view class="userName loginBoxItem">
+      <span>用户名</span>
+      <input type="text">
+    </view>
+    <view class="password loginBoxItem">
+      <span>密码</span>
+      <input type="text">
+    </view>
+    <button>登录</button>
+  </view>
+</template>
+<script lang="ts">
+
+</script>
+<style lang="scss" scoped>
+.page-container{
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #f5f5f5;
+  .title{
+    margin-top: 200px;
+    font-size: 30px;
+  }
+  input{
+    border:1px solid #dcdfe6;
+  }
+  .loginBoxItem{
+    display: flex;
+    margin-top:20px;
+    width: 240px;
+    justify-content: space-between;
+  }
+  button{
+    background-color: #409eff;
+    margin-top: 30px;
+    padding:4px 26px;
+    color:white;
+    font-size: 16px;
+  }
+}
+</style>
