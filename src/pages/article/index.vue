8fa298b94ff4be3a15927bfa4730be917812e96d
杨泽 <1083849984@qq.com>
2021-02-26 10:57:01 +0800

    this为undefined

--- src/pages/article/index.vue
+++ src/pages/article/index.vue
@@ -2,8 +2,8 @@
   <view class="page">
     <uni-list>
       <uni-list-item v-for="article in articleList"
-                   :title="article" clickable
-                   @click="pop">
+                     :title="article" clickable
+                     @click="pop">
       </uni-list-item>
     </uni-list>
     <uni-popup ref="popup" type="center">hi</uni-popup>
@@ -12,23 +12,16 @@
 
 <script lang="ts">
 
-import UniListItem from "@/components/uni-list-item/uni-list-item.vue";
-export default {
-  components: {UniListItem},
-  data() {
-    return {
-      articleList: ['家', '春', '秋']
-    }
-  },
-  methods: {
-    pop() {
-      this.$refs.popup.open()
-    }
+import Vue from 'vue'
+
+export default class Article extends Vue {
+  articleList: string[] = ['家', '春', '秋']
+
+  pop() {
+    console.log(this);
+    // this.$refs.popup.open()
   }
 }
 </script>
 <style lang="scss" scoped>
-.uni-tabbar {
-  display: none;
-}
 </style>
